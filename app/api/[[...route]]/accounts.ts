4fb164c135b4af31861df2253a382bdf556d1b9c
ArfinHasib <arfinhasib21375@gmail.com>
2024-06-10 13:21:38 +0600

    added shadcn form

--- app/api/[[...route]]/accounts.ts
+++ app/api/[[...route]]/accounts.ts
@@ -1,28 +1,60 @@
 import { Hono } from 'hono';
 
 import { db } from '@/db/drizzle';
-import { accounts } from '@/db/schema';
+import { accounts, inserertAccountSchema } from '@/db/schema';
 
-import { HTTPException } from 'hono/http-exception';
+import { zValidator } from '@hono/zod-validator';
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 import { eq } from 'drizzle-orm';
 
-const app = new Hono().get('/', clerkMiddleware(), async (c) => {
-   const auth = getAuth(c);
+import { createId } from '@paralleldrive/cuid2';
 
-   if (!auth?.userId) {
-      return c.json({ error: 'Unauthorized' }, 401);
-   }
+const app = new Hono()
+   .get('/', clerkMiddleware(), async (c) => {
+      const auth = getAuth(c);
 
-   const data = await db
-      .select({
-         id: accounts.id,
-         name: accounts.name,
-      })
-      .from(accounts)
-      .where(eq(accounts.userId, auth.userId));
+      if (!auth?.userId) {
+         return c.json({ error: 'Unauthorized' }, 401);
+      }
 
-   return c.json({ data });
-});
+      const data = await db
+         .select({
+            id: accounts.id,
+            name: accounts.name,
+         })
+         .from(accounts)
+         .where(eq(accounts.userId, auth.userId));
+
+      return c.json({ data });
+   })
+   .post(
+      '/',
+      clerkMiddleware(),
+      zValidator(
+         'json',
+         inserertAccountSchema.pick({
+            name: true,
+         })
+      ),
+      async (c) => {
+         const auth = getAuth(c);
+         const values = c.req.valid('json');
+
+         if (!auth?.userId) {
+            return c.json({ error: 'Unauthorized' }, 401);
+         }
+
+         const [data] = await db
+            .insert(accounts)
+            .values({
+               id: createId(),
+               userId: auth.userId,
+               ...values,
+            })
+            .returning();
+
+         return c.json({ data });
+      }
+   );
 
 export default app;
--- features/accounts/api/use-get.accounts.ts
+++ features/accounts/api/use-get.accounts.ts
@@ -17,5 +17,4 @@ export const useGetAccounts = () => {
       },
    });
    return query;
-   // asf
 };
