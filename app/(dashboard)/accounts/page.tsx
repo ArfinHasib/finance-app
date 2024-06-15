e413c4eb06309a239fbaa292e01e74a0bed87ab6
ArfinHasib <arfinhasib21375@gmail.com>
2024-06-16 00:41:01 +0600

    added shadcn table and tanstack react table

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -1,8 +1,27 @@
+'use client';
+
+import { useNewAccount } from '@/features/accounts/hooks/use-new-accounts';
+
+import { Button } from '@/components/ui/button';
+import { Card, CardHeader, CardTitle } from '@/components/ui/card';
+import { Plus } from 'lucide-react';
+
 const AccountsPage = () => {
+    const newAccount = useNewAccount();
+
     return (
-        <Card>
-            <div>Accounts Page</div>
-        </Card>
+        <div className='max-w-screen-2xl mx-auto w-full pb-10 -mt-24'>
+            <Card className='border-none drop-shadow-sm'>
+                <CardHeader className='gap-y-2 lg:flex-row lg:items-center lg:justify-between'>
+                    <CardTitle className='text-xl line-clamp-1'>
+                        Accounts Page
+                    </CardTitle>
+                    <Button size='sm' onClick={newAccount.onOpen}>
+                        <Plus className='size-4 mr-2' /> Add New
+                    </Button>
+                </CardHeader>
+            </Card>
+        </div>
     );
 };
 
