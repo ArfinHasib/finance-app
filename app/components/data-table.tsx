ef2ab06b67bfc1894ca2ddcb4eaeb1e35e65132b
ArfinHasib <arfinhasib21375@gmail.com>
2024-06-19 23:19:45 +0600

    Finalize Accounts Bulk Delete

--- app/components/data-table.tsx
+++ app/components/data-table.tsx
@@ -23,6 +23,7 @@ import {
     TableHeader,
     TableRow,
 } from '@/components/ui/table';
+import { useConfirm } from '@/hooks/use-confirm';
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[];
@@ -43,6 +44,11 @@ export function DataTable<TData, TValue>({
     onDelete,
     disabled,
 }: DataTableProps<TData, TValue>) {
+    const [ConfirmDialog, confirm] = useConfirm(
+        'Are your sure?',
+        'You are about to perform a bulk delete'
+    );
+
     const [sorting, setSorting] = React.useState<SortingState>([]);
     const [columnFilters, setColumnFilters] =
         React.useState<ColumnFiltersState>([]);
@@ -67,6 +73,7 @@ export function DataTable<TData, TValue>({
 
     return (
         <div>
+            <ConfirmDialog />
             <div className='flex items-center py-4'>
                 <Input
                     placeholder={`Filter ${filterKey}`}
@@ -88,9 +95,15 @@ export function DataTable<TData, TValue>({
                         size='sm'
                         variant='outline'
                         className='ml-auto font-normal text-xs'
-                        onClick={() => {
-                            onDelete(table.getFilteredSelectedRowModel().rows);
-                            table.resetRowSelection();
+                        onClick={async () => {
+                            const ok = await confirm();
+
+                            if (ok) {
+                                onDelete(
+                                    table.getFilteredSelectedRowModel().rows
+                                );
+                                table.resetRowSelection();
+                            }
                         }}
                     >
                         <Trash className='size-4 mr-2' />
